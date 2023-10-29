1109acba3ffb992c83930040780d2ee7a11a43c3
timaevV <timaevvladislav@gmail.com>
2023-10-30 01:43:48 +0600

    fix:interface message

--- client/src/components/LongPulling.tsx
+++ client/src/components/LongPulling.tsx
@@ -1,14 +1,36 @@
-import React, {ChangeEvent, useState} from "react"
+import React, {ChangeEvent, useEffect, useState} from "react"
 import {v4 as uuidv4} from "uuid"
 import axios from "../api"
 
+interface IMessage {
+    id: string
+    message: string
+}
+
 const LongPulling = () => {
-    const [messages, setMessages] = useState<[]>([])
+    const [messages, setMessages] = useState<any>([])
     const [value, setValue] = useState<string>("")
 
-    const sendMessage: () => Promise<[]> = async () => {
-        const response = await axios.post("/api/v1/message", {message: value, id: uuidv4()})
-        return []
+    useEffect(() => {
+        subscribe()
+    }, [])
+
+    const subscribe: () => void = async () => {
+       try {
+           const {data} = await axios.get("/api/v1/message")
+           setMessages((prev: []) => [data, ...prev])
+       } catch (e) {
+           setTimeout(() => subscribe(), 500)
+       }
+    }
+
+    const sendMessage: () => Promise<string> = async () => {
+        try {
+            const response = await axios.post("/api/v1/message", {message: value, id: uuidv4()})
+            return response.data
+        } catch (e) {
+            console.error(e)
+        }
     }
 
     return (
@@ -24,10 +46,10 @@ const LongPulling = () => {
                 <button onClick={sendMessage}>
                     Отправить
                 </button>
+            </div>
 
-                <div className="messages">
-                    {messages.map((m: any, index) => <div className="message" key={m.id}>{m.message}</div>)}
-                </div>
+            <div className="messages">
+              {messages.map((m: IMessage, index: number) => <div className="message" key={m.id}>{m.message}</div>)}
             </div>
            </>
         </div>
