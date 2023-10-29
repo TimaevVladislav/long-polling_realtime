02763028a773e60e9ce5dcec70127a65ffb7c14a
timaevV <timaevvladislav@gmail.com>
2023-10-30 01:04:16 +0600

    feat:axios api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react"
+import LongPulling from "./components/LongPulling"
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <>
+     <LongPulling />
+    </>
   )
 }
 
