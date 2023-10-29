6c77fb67f1edd682f21dc8a7585b1f337f1ddd87
timaevV <timaevvladislav@gmail.com>
2023-10-30 01:04:02 +0600

    feat:long pulling

--- client/src/components/LongPulling.tsx
+++ client/src/components/LongPulling.tsx
@@ -1,9 +1,34 @@
-import React from "react"
+import React, {ChangeEvent, useState} from "react"
+import axios from "../api"
 
 const LongPulling = () => {
-    return (
-        <div>
+    const [messages, setMessages] = useState<[]>([])
+    const [value, setValue] = useState<string>("")
 
+    const sendMessage: () => Promise<[]> = async () => {
+        const response = await axios.post("")
+        return []
+    }
+
+    return (
+        <div className="center">
+           <>
+            <div className="form">
+                <input
+                    value={value}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+                    type="text"
+                />
+
+                <button onClick={sendMessage}>
+                    Отправить
+                </button>
+
+                <div className="messages">
+                    {messages.map((m: any, index) => <div className="message" key={m.id}>{m.message}</div>)}
+                </div>
+            </div>
+           </>
         </div>
     )
 }
